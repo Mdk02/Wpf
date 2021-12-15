3539aca75c1cacfa9d474bb5ceebe0f3acd350b2
Mdk02 <90508898+Mdk02@users.noreply.github.com>
2021-12-15 23:50:18 +0500

    SaveUser

--- WpfApp1/Autoriz.xaml.cs
+++ WpfApp1/Autoriz.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,20 +19,26 @@ namespace WpfApp1
     public partial class Autoriz : Window
     {
         List<User> users;
+        string path = @"C:\Users\1\Desktop\2\WpfApp1\Data\LastUser.txt";
         public Autoriz()
         {
             InitializeComponent();
 
             Closing += CloseWindow;
+
+            ShowSave();
         }
 
         //Enter
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            users = new DataMethodsUser().GetAllDataUsers().ToList();
+            users = new DataMethodsUser().GetAllDataUsers().ToList();            
 
             if (!string.IsNullOrEmpty(loginTextBox.Text) && !string.IsNullOrEmpty(passwordTextBox.Password))
             {
+                MessageBox.Show(loginTextBox.Text);
+                MessageBox.Show(passwordTextBox.Password);
+
                 var user = users.FirstOrDefault(u => u.Login.Equals(loginTextBox.Text) && u.Password.Equals(passwordTextBox.Password));
 
                 if (user == default)
@@ -41,6 +48,7 @@ namespace WpfApp1
                 else
                 {
                     MainWindow.Current_user = user;
+                    Save(user);
                     Closing -= CloseWindow;
                     Close();
                 }
@@ -62,11 +70,35 @@ namespace WpfApp1
             Application.Current.Shutdown();
         }
 
-        void Save()
+        void Save(User user)
         {
             if (SavePassword.IsChecked.Value)
-            {
+            {               
+                File.WriteAllLines(path, new string[] { user.Login, user.Password });
+            }
+        }
 
+        void ShowSave()
+        {
+            string log = "", pass = "";
+
+            using (StreamReader sw = new StreamReader(path))
+            {
+                string line;
+                List<string> lines = new List<string>();
+                while ((line = sw.ReadLine()) != null)
+                {
+                    lines.Add(line);
+                }
+
+                log = lines[0];
+                pass = lines[1];
+            }
+
+            if (!string.IsNullOrEmpty(log))
+            {
+                loginTextBox.Text = log;
+                passwordTextBox.Password = pass;
             }
         }
     }
